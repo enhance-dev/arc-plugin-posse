4274b37b1c7212957e73fb477ac1fecee6d48e97
macdonst <simon.macdonald@gmail.com>
2023-04-18 23:09:01 -0400

    Parse rss feed
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/plugins/arc-plugin-posse/scheduled/check-rss-feed/index.js
+++ src/plugins/arc-plugin-posse/scheduled/check-rss-feed/index.js
@@ -1,7 +1,33 @@
-async function handler (event) {
-  console.log(JSON.stringify(event, null, 2))
-  console.log(process.env.RSS_URL)
+const http = require('http')
+const https = require('https')
+const { parseStringPromise } = require('xml2js')
+
+async function handler () {
+  const feed = new URL(process.env.RSS_URL)
+  const response = await getFeed(feed)
+  const result = await parseStringPromise(response)
+  const items = result?.rss?.channel[0]?.item || []
+  console.log(items)
   return
+}
+
+function getFeed (feed) {
+  const client = feed.protocol === 'http:' ? http : https
+  return new Promise((resolve, reject) => {
+    const req = client.request(feed, (res) => {
+      let responseBody = ''
+      res.on('data', (chunk) => {
+        responseBody += chunk
+      })
+      res.on('end', () => {
+        resolve(responseBody)
+      })
+    })
+    req.on('error', (err) => {
+      reject(err)
+    })
+    req.end()
+  })
 }
 
 module.exports = { handler }
