0409d2907f512535aafaccf72bcddf67a17b28e2
macdonst <simon.macdonald@gmail.com>
2023-04-21 16:02:32 -0400

    Remove V1 prefix
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/plugins/arc-plugin-posse/events/post-to-twitter/index.js
+++ src/plugins/arc-plugin-posse/events/post-to-twitter/index.js
@@ -15,8 +15,8 @@ ${desc}
 ${link[0]}`
 
   const client = new TwitterApi({
-    appKey: process.env.V1_TWITTER_API_KEY,
-    appSecret: process.env.V1_TWITTER_API_SECRET,
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
     accessToken: process.env.TWITTER_ACCESS_TOKEN,
     accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   })
